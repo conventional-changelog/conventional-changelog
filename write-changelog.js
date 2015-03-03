3bb5a25ac2a814591e5d65fe4430f7a3055d5570
Andrew <andrewtjoslin@gmail.com>
2015-03-02 18:50:59 -0700

    release: v0.0.13

--- write-changelog.js
+++ write-changelog.js
@@ -6,7 +6,6 @@ var changelog = require('./index.js');
 
 changelog({
   version: pkg.version,
-  subtitle: '"' + pkg.codename + '"',
   repository: 'https://github.com/ajoslin/conventional-changelog'
 }, function(err, log) {
   if (err) {
