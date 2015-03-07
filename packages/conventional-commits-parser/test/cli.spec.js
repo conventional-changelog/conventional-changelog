7b9280be8139903abc78dcbcf01ae4884ad0dff9
Steve Mao <maochenyan@gmail.com>
2015-03-08 01:09:38 +1100

    test(cli): fix test on iojs

--- packages/conventional-commits-parser/test/cli.spec.js
+++ packages/conventional-commits-parser/test/cli.spec.js
@@ -1,8 +1,9 @@
 'use strict';
+var concat = require('concat-stream');
 var expect = require('chai').expect;
 var readFileSync = require('fs').readFileSync;
 var spawn = require('child_process').spawn;
-var concat = require('concat-stream');
+var through = require('through2');
 
 var cliPath = './cli.js';
 var output1 = readFileSync('test/expected/output1.txt', 'utf-8');
@@ -11,10 +12,8 @@ var output2 = readFileSync('test/expected/output2.txt', 'utf-8');
 describe('cli', function() {
   it('should parse commits in a file', function(done) {
     var cp = spawn(cliPath, ['test/fixtures/log.txt']);
-
     cp.stdout
       .pipe(concat(function(chunk) {
-
         expect(chunk.toString()).to.equal(output1);
         done();
       }));
@@ -22,10 +21,8 @@ describe('cli', function() {
 
   it('should work with a separator', function(done) {
     var cp = spawn(cliPath, ['test/fixtures/log2.txt', '===']);
-
     cp.stdout
       .pipe(concat(function(chunk) {
-
         expect(chunk.toString()).to.equal(output2);
         done();
       }));
@@ -33,21 +30,27 @@ describe('cli', function() {
 
   it('should work with two files', function(done) {
     var cp = spawn(cliPath, ['test/fixtures/log.txt', 'test/fixtures/log2.txt', '===']);
-
     cp.stdout
       .pipe(concat(function(chunk) {
         var expected = output1 + output2;
-
         expect(chunk.toString()).to.equal(expected);
         done();
       }));
   });
 
   it('should error if files cannot be found', function(done) {
+    var i = 0;
     var cp = spawn(cliPath, ['test/fixtures/log.txt', 'test/fixtures/log3.txt', 'test/fixtures/log2.txt', 'test/fixtures/log4.txt', '===']);
     cp.stderr
-      .pipe(concat(function(chunk) {
-        expect(chunk.toString()).to.equal('Failed to read file test/fixtures/log3.txt\nError: ENOENT, open \'test/fixtures/log3.txt\'\nFailed to read file test/fixtures/log4.txt\nError: ENOENT, open \'test/fixtures/log4.txt\'\n');
+      .pipe(through(function(chunk, enc, cb) {
+        if (i === 0) {
+          expect(chunk.toString()).to.contain('Failed to read file test/fixtures/log3.txt');
+        } else {
+          expect(chunk.toString()).to.contain('Failed to read file test/fixtures/log4.txt');
+        }
+        i++;
+        cb();
+      }, function() {
         done();
       }));
   });
