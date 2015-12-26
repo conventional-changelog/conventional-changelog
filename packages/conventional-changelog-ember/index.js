248865015b285f007ce86ccf3ad5b59deccc48c7
Steve Mao <maochenyan@gmail.com>
2015-12-26 18:25:08 +1100

    Extracting code from https://github.com/ajoslin/conventional-changelog

---
+++ packages/conventional-changelog-ember/index.js
@@ -0,0 +1,76 @@
+'use strict';
+var Q = require('q');
+var readFile = Q.denodeify(require('fs').readFile);
+var resolve = require('path').resolve;
+
+var bodyPattern = /^\[(.*) (.*)] (.*)$/;
+
+function presetOpts(cb) {
+  var parserOpts = {
+    headerPattern: /^Merge pull request #(.*) from .*$/,
+    headerCorrespondence: [
+      'pr'
+    ]
+  };
+
+  var writerOpts = {
+    transform: function(commit) {
+      if (!commit.pr) {
+        return;
+      }
+
+      var header = commit.body || commit.footer;
+
+      var match = header ? header.match(bodyPattern) : null;
+
+      if (!match) {
+        return;
+      }
+
+      commit.tag = match[1];
+      commit.taggedAs = match[2];
+      commit.message = match[3];
+
+      if (commit.tag === 'BUGFIX') {
+        commit.tag = 'Bug Fixes';
+      } else if (commit.tag === 'CLEANUP') {
+        commit.tag = 'Cleanup';
+      } else if (commit.tag === 'FEATURE') {
+        commit.tag = 'Features';
+      } else if (commit.tag === 'DOC') {
+        commit.tag = 'Documentation';
+      } else if (commit.tag === 'SECURITY') {
+        commit.tag = 'Security';
+      } else {
+        return;
+      }
+
+      if (typeof commit.hash === 'string') {
+        commit.hash = commit.hash.substring(0, 7);
+      }
+
+      return commit;
+    },
+    groupBy: 'tag',
+    commitGroupsSort: 'title',
+    commitsSort: ['tag', 'taggedAs', 'message']
+  };
+
+  Q.all([
+    readFile(resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
+    readFile(resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
+    readFile(resolve(__dirname, 'templates/commit.hbs'), 'utf-8')
+  ])
+    .spread(function(template, header, commit) {
+      writerOpts.mainTemplate = template;
+      writerOpts.headerPartial = header;
+      writerOpts.commitPartial = commit;
+
+      cb(null, {
+        parserOpts: parserOpts,
+        writerOpts: writerOpts
+      });
+    });
+}
+
+module.exports = presetOpts;
---
+++ packages/conventional-changelog-ember/readme.md
@@ -0,0 +1,16 @@
+#  [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage Status][coveralls-image]][coveralls-url]
+
+> [conventional-changelog](https://github.com/ajoslin/conventional-changelog) [ember](https://github.com/emberjs/ember.js) preset
+
+
+See [convention](convention.md)
+
+
+[npm-image]: https://badge.fury.io/js/conventional-changelog-ember.svg
+[npm-url]: https://npmjs.org/package/conventional-changelog-ember
+[travis-image]: https://travis-ci.org/stevemao/conventional-changelog-ember.svg?branch=master
+[travis-url]: https://travis-ci.org/stevemao/conventional-changelog-ember
+[daviddm-image]: https://david-dm.org/stevemao/conventional-changelog-ember.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/stevemao/conventional-changelog-ember
+[coveralls-image]: https://coveralls.io/repos/stevemao/conventional-changelog-ember/badge.svg
+[coveralls-url]: https://coveralls.io/r/stevemao/conventional-changelog-ember
