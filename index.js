65a9f4bfcfb2621fbcc9eed8876dd22e96a55ed0
Steve Mao <maochenyan@gmail.com>
2016-01-30 23:39:08 +1100

    fix(bin): no cli anymore

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 var conventionalChangelogCore = require('conventional-changelog-core');
 
 function conventionalChangelog(options, context, gitRawCommitsOpts, parserOpts, writerOpts) {
-  var loadPreset;
-
   options.warn = options.warn || function() {};
 
   if (options.preset) {
     try {
       options.config = require('conventional-changelog-' + options.preset.toLowerCase());
-      loadPreset = true;
     } catch (err) {
-      loadPreset = false;
       options.warn('Preset: "' + options.preset + '" does not exist');
     }
   }
