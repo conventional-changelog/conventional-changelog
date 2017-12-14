9be30282ca515e7da1adb9faaf376ef27ac201e0
dale tan <dale@1stdibs.com>
2017-12-14 16:48:43 -0500

    feat: indicate patch/fix output for bump
    - added more meta data for downstream usage

--- packages/conventional-recommended-bump/presets/angular.js
+++ packages/conventional-recommended-bump/presets/angular.js
@@ -3,6 +3,7 @@ var presetOpts = {
     var level = 2;
     var breakings = 0;
     var features = 0;
+    var fixes = 0;
 
     commits.forEach(function(commit) {
       if (commit.notes.length > 0) {
@@ -13,12 +14,22 @@ var presetOpts = {
         if (level === 2) {
           level = 1;
         }
+      } else if (commit.type === 'fix') {
+        // no need to reset `level` since it is
+        // inherently a patch and it would be
+        // preferable to take the highest level bump
+        fixes += 1;
       }
     });
 
     return {
       level: level,
-      reason: 'There are ' + breakings + ' BREAKING CHANGES and ' + features + ' features'
+      reason: 'There are ' + breakings + ' BREAKING CHANGES, ' + features + ' features, and ' + fixes + ' fixes',
+      changes: {
+        breaking: breakings,
+        features: features,
+        fixes: fixes
+      }
     };
   },
   parserOpts: {
