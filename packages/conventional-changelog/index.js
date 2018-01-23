59682d181017fc2e88070ee8f1fecfa245d38a01
fenduru <fenduru@users.noreply.github.com>
2018-01-23 18:56:33 -0500

    fix(presets): support scoped packages as presets
    Previously, a scoped packages such as `@my-org/conventional-changelog-myorg` was not able to be used when specifying a preset. This change checks if the preset is scoped, and re-appends the scope when constructing the preset package name.

--- packages/conventional-changelog/index.js
+++ packages/conventional-changelog/index.js
@@ -6,7 +6,17 @@ function conventionalChangelog(options, context, gitRawCommitsOpts, parserOpts, 
 
   if (options.preset) {
     try {
-      options.config = require('conventional-changelog-' + options.preset.toLowerCase());
+      var scope = '';
+      var name = options.preset.toLowerCase();
+
+      if (name[0] === '@') {
+        var parts = name.split('/');
+        scope = parts.shift() + '/';
+        name = parts.join('');
+      }
+      
+      console.log('scope', scope, 'name', name);
+      options.config = require(scope + 'conventional-changelog-' + name);
     } catch (err) {
       options.warn('Preset: "' + options.preset + '" does not exist');
     }
