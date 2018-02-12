0f08267a09f3406d5c51d9a5e207535db6b72736
Hutson Betts <hutson@hyper-expanse.net>
2018-02-12 09:35:34 -0600

    fix(conventional-changelog): support scoped presets
    Add support for scoped preset packages, such as `@scope/angular`.

--- packages/conventional-changelog/index.js
+++ packages/conventional-changelog/index.js
@@ -1,12 +1,13 @@
 'use strict';
 var conventionalChangelogCore = require('conventional-changelog-core');
+var conventionalChangelogPresetLoader = require('conventional-changelog-preset-loader');
 
 function conventionalChangelog(options, context, gitRawCommitsOpts, parserOpts, writerOpts) {
   options.warn = options.warn || function() {};
 
   if (options.preset) {
     try {
-      options.config = require('conventional-changelog-' + options.preset.toLowerCase());
+      options.config = conventionalChangelogPresetLoader(options.preset.toLowerCase());
     } catch (err) {
       options.warn('Preset: "' + options.preset + '" does not exist');
     }
