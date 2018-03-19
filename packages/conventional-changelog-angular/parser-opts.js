b167b69dc0956c2717875dc10ce42c2bacfcf00e
Felix Wu <mail@flxwu.com>
2018-03-19 17:49:11 +0100

    refactor(angular): adapt BREAKING CHANGE keyword to Angular convention
    BREAKING CHANGE:
      The Angular conventions specifically say that breaking changes must
      start with "BREAKING CHANGE", not the plural form. Therefore the
      previous plural form "CHANGES" has been corrected to singular "CHANGE".

--- packages/conventional-changelog-angular/parser-opts.js
+++ packages/conventional-changelog-angular/parser-opts.js
@@ -7,7 +7,7 @@ module.exports = {
     `scope`,
     `subject`
   ],
-  noteKeywords: [`BREAKING CHANGE`, `BREAKING CHANGES`],
+  noteKeywords: [`BREAKING CHANGE`],
   revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
   revertCorrespondence: [`header`, `hash`]
 }
