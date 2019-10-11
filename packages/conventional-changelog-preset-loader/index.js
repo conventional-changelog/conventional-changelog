83ed4f262110c94fe5ddd69b6bea03cb13bda447
merceyz <merceyz@users.noreply.github.com>
2019-10-11 23:26:32 +0200

    fix(preset-loader): handle absolute name

--- packages/conventional-changelog-preset-loader/index.js
+++ packages/conventional-changelog-preset-loader/index.js
@@ -7,24 +7,29 @@ module.exports.presetLoader = presetLoader
 
 function presetLoader (requireMethod) {
   return path => {
-    let isAbsolute = false
     let name = ''
     let scope = ''
+    let absolutePath = ''
 
     if (typeof path === 'string') {
       name = path.toLowerCase()
-      isAbsolute = nodePath.isAbsolute(path)
+      if (nodePath.isAbsolute(path)) {
+        absolutePath = path
+      }
     } else if (typeof path === 'object' && path.name) {
       // Rather than a string preset name, options.preset can be an object
       // with a "name" key indicating the preset to load; additinoal key/value
       // pairs are assumed to be configuration for the preset. See the documentation
       // for a given preset for configuration available.
       name = path.name.toLowerCase()
+      if (nodePath.isAbsolute(path.name)) {
+        absolutePath = path.name
+      }
     } else {
       throw Error('preset must be string or object with key name')
     }
 
-    if (!isAbsolute) {
+    if (!absolutePath) {
       if (name[0] === `@`) {
         const parts = name.split(`/`)
         scope = parts.shift() + `/`
@@ -37,7 +42,7 @@ function presetLoader (requireMethod) {
     }
 
     try {
-      const config = requireMethod(isAbsolute ? path : `${scope}${name}`)
+      const config = requireMethod(absolutePath || `${scope}${name}`)
       // rather than returning a promise, presets can return a builder function
       // which accepts a config object (allowing for customization) and returns
       // a promise.
