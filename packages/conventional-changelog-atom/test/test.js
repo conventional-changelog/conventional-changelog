9f010e1a9767716585b28f71aef2f5c5c4bc74c7
Tomasz Wojtun <Tomasz.Wojtun@stepstone.com>
2019-11-24 15:47:05 +0100

    test fix

--- packages/conventional-changelog-atom/test/test.js
+++ packages/conventional-changelog-atom/test/test.js
@@ -6,6 +6,7 @@ var describe = mocha.describe
 var it = mocha.it
 var before = mocha.before
 var expect = require('chai').expect
+var gitDummyCommit = require('git-dummy-commit')
 var shell = require('shelljs')
 var through = require('through2')
 var writeFileSync = require('fs').writeFileSync
@@ -19,17 +20,11 @@ describe('atom preset', function () {
     shell.cd('tmp')
     shell.mkdir('git-templates')
     shell.exec('git init --template=./git-templates')
-
-    writeFileSync('test1', '')
-    shell.exec('git add --all && git commit -m":arrow_down: exception-reporting"')
-    writeFileSync('test2', '')
-    shell.exec('git add --all && git commit -m\':bug: "updateContentDimensions" when model changes\'')
-    writeFileSync('test3', '')
-    shell.exec('git add --all && git commit -m"Merge pull request #7881 from atom/bf-upgrade-babel-to-5.6.17"')
-    writeFileSync('test4', '')
-    shell.exec('git add --all && git commit -m":arrow_up: language-gfm@0.79.0"')
-    writeFileSync('test5', '')
-    shell.exec('git add --all && git commit -m":arrow_up: one-dark/light-ui@v1.0.1"')
+    gitDummyCommit([':arrow_down: exception-reporting'])
+    gitDummyCommit([':bug: `updateContentDimensions` when model changes'])
+    gitDummyCommit(['Merge pull request #7881 from atom/bf-upgrade-babel-to-5.6.17'])
+    gitDummyCommit([':arrow_up: language-gfm@0.79.0'])
+    gitDummyCommit([':arrow_up: one-dark/light-ui@v1.0.1'])
   })
 
   it('should work if there is no semver tag', function (done) {
@@ -43,7 +38,7 @@ describe('atom preset', function () {
         chunk = chunk.toString()
 
         expect(chunk).to.include(':arrow_down:')
-        expect(chunk).to.include('"updateContentDimensions" when model changes')
+        expect(chunk).to.include('`updateContentDimensions` when model changes')
         expect(chunk).to.include(':arrow_up:')
         expect(chunk).to.include('one-dark/light-ui@v1.0.1')
 
