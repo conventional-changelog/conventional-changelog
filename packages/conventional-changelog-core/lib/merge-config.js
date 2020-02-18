ee92478a49090e09897de3a0b0d14036b1a6dbb0
Kelly Selden <kellyselden@gmail.com>
2020-02-18 15:06:09 +0000

    fix(conventional-changelog-core): send `options.path` to `semverTagsPromise`
    This option is already documented in the README

--- packages/conventional-changelog-core/lib/merge-config.js
+++ packages/conventional-changelog-core/lib/merge-config.js
@@ -21,7 +21,7 @@ const rhosts = /github|bitbucket|gitlab/i
 
 function semverTagsPromise (options) {
   return Q.Promise(function (resolve, reject) {
-    gitSemverTags({ lernaTags: !!options.lernaPackage, package: options.lernaPackage, tagPrefix: options.tagPrefix, skipUnstable: options.skipUnstable }, function (err, result) {
+    gitSemverTags({ lernaTags: !!options.lernaPackage, package: options.lernaPackage, tagPrefix: options.tagPrefix, skipUnstable: options.skipUnstable, cwd: options.path }, function (err, result) {
       if (err) {
         reject(err)
       } else {
