fc11e825ba1c3a8b26baa5fcf62b72074fe6d244
Jon Senchyna <TheSench@users.noreply.github.com>
2022-10-01 08:04:01 -0400

    test: Add tests for multiple path filters

--- packages/git-raw-commits/test.js
+++ packages/git-raw-commits/test.js
@@ -227,4 +227,60 @@ describe('git-raw-commits', function () {
           }))
       })
   })
+
+  it('should allow commits to be scoped to a specific set of directories', function (done) {
+    fs.mkdirSync('./packages/common', { recursive: true })
+    writeFileSync('./packages/foo/test1', 'update')
+    exec('git add --all && git commit -m"Fifth commit"')
+    writeFileSync('test2', 'update')
+    exec('git add --all && git commit -m"Sixth commit"')
+    writeFileSync('./packages/common/test1', 'hello')
+    exec('git add --all && git commit -m"Seventh commit"')
+
+    let chunks = []
+    gitRawCommits({
+      paths: ['./packages/foo', './packages/common'],
+    })
+      .pipe(through(function (chunk, enc, cb) {
+        chunks.push(chunk.toString())
+        cb()
+      }, function () {
+        expect(chunks).to.have.ordered.members([
+          'Seventh commit\n\n',
+          'Fifth commit\n\n',
+          'Fourth commit\n\n',
+          'First commit\n\n',
+        ])
+        done()
+      }))
+  })
+
+  it('should allow commits to be scoped using both path and paths', function (done) {
+    try {
+      fs.mkdirSync('./packages/other', { recursive: true })
+      writeFileSync('./packages/other/test1', 'hello')
+      exec('git add --all && git commit -m"Eighth commit"')
+    } catch(e) {
+      fail(e)
+    }
+
+    let chunks = []
+    gitRawCommits({
+      path: './packages/foo',
+      paths: ['./packages/other', './packages/common'],
+    })
+      .pipe(through(function (chunk, enc, cb) {
+        chunks.push(chunk.toString())
+        cb()
+      }, function () {
+        expect(chunks).to.have.ordered.members([
+          'Eighth commit\n\n',
+          'Seventh commit\n\n',
+          'Fifth commit\n\n',
+          'Fourth commit\n\n',
+          'First commit\n\n',
+        ])
+        done()
+      }))
+  })
 })
