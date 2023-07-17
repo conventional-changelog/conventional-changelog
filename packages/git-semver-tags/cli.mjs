b66f80b22bf6480f01afacaf91af35e81f9e764a
Patrick Davies <patrick.davies@dialogue.co>
2023-07-17 12:23:17 -0400

    fix indent

--- packages/git-semver-tags/cli.mjs
+++ packages/git-semver-tags/cli.mjs
@@ -10,7 +10,7 @@ const args = meow(`
     --lerna                parse lerna style git tags
     --package <name>       when listing lerna style tags, filter by a package
     --tag-prefix <prefix>  prefix to remove from the tags during their processing
-    --skip-unstable                If given, unstable tags will be skipped, e.g., x.x.x-alpha.1, x.x.x-rc.2`,
+    --skip-unstable        if given, unstable tags will be skipped, e.g., x.x.x-alpha.1, x.x.x-rc.2`,
 {
   importMeta: import.meta,
   booleanDefault: undefined,
