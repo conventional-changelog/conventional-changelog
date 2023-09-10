55079bfe457b1f96f7c587345d0798cc787c05c2
dangreen <danon0404@gmail.com>
2023-09-11 01:41:43 +0400

    feat!: fix transform async handlers

--- packages/gulp-conventional-changelog/index.js
+++ packages/gulp-conventional-changelog/index.js
@@ -20,47 +20,52 @@ module.exports = function (opts, context, gitRawCommitsOpts, parserOpts, writerO
   return new Transform({
     objectMode: true,
     highWaterMark: 16,
-    async transform  (file, enc, cb) {
-      if (file.isNull()) {
-        cb(null, file)
-        return
-      }
-
-      const { default: conventionalChangelog } = await conventionalChangelogPromise
-      const stream = conventionalChangelog(opts, context, gitRawCommitsOpts, parserOpts, writerOpts)
-        .on('error', function (err) {
-          cb(new PluginError('gulp-conventional-changelog', err))
-        })
-
-      if (file.isStream()) {
-        if (opts.releaseCount === 0) {
-          file.contents = stream
-        } else if (opts.append) {
-          file.contents = file.contents
-            .pipe(addStream(stream))
-        } else {
-          file.contents = stream
-            .pipe(addStream(file.contents))
+    // `transform` option should not return Promises.
+    // It cause a bug in Node.js 16, because it interprets the Promise resolve as a callback call.
+    // In Node 20 it handle only callback call, Promises is not handled.
+    transform  (file, enc, cb) {
+      (async () => {
+        if (file.isNull()) {
+          cb(null, file)
+          return
         }
 
-        cb(null, file)
-        return
-      }
+        const { default: conventionalChangelog } = await conventionalChangelogPromise
+        const stream = conventionalChangelog(opts, context, gitRawCommitsOpts, parserOpts, writerOpts)
+          .on('error', function (err) {
+            cb(new PluginError('gulp-conventional-changelog', err))
+          })
 
-      stream
-        .pipe(concat({
-          encoding: 'buffer'
-        }, function (data) {
+        if (file.isStream()) {
           if (opts.releaseCount === 0) {
-            file.contents = data
+            file.contents = stream
           } else if (opts.append) {
-            file.contents = Buffer.concat([file.contents, data])
+            file.contents = file.contents
+              .pipe(addStream(stream))
           } else {
-            file.contents = Buffer.concat([data, file.contents])
+            file.contents = stream
+              .pipe(addStream(file.contents))
           }
 
           cb(null, file)
-        }))
+          return
+        }
+
+        stream
+          .pipe(concat({
+            encoding: 'buffer'
+          }, function (data) {
+            if (opts.releaseCount === 0) {
+              file.contents = data
+            } else if (opts.append) {
+              file.contents = Buffer.concat([file.contents, data])
+            } else {
+              file.contents = Buffer.concat([data, file.contents])
+            }
+
+            cb(null, file)
+          }))
+      })()
     }
   })
 }
