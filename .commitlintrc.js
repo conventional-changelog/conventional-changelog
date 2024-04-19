248fbf936e7bc22746950e0276928629c395380b
Dan Onoshko <danon0404@gmail.com>
2024-04-19 18:09:39 +0400

    ci: tweak workflows and add git checks (#1230)

---
+++ .commitlintrc.js
@@ -0,0 +1,29 @@
+import scopes from '@commitlint/config-pnpm-scopes'
+
+export default {
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-pnpm-scopes'
+  ],
+  rules: {
+    'body-max-line-length': [0],
+    'scope-enum': async (ctx) => {
+      const scopeEnum = await scopes.rules['scope-enum'](ctx)
+
+      return [
+        scopeEnum[0],
+        scopeEnum[1],
+        [
+          ...scopeEnum[2],
+          'deps',
+          'dev-deps'
+        ]
+      ]
+    }
+  },
+  prompt: {
+    settings: {
+      enableMultipleScopes: true
+    }
+  }
+}
