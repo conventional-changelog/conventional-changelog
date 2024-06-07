1e839f96049a6d870e7cf48d9e72900320e29d6e
MHDFahz <fahis.skazi@gmail.com>
2024-06-07 12:58:55 +0530

    feat: prevent modifications to Date objects in commit.ts
    This change adds a condition in the `preventModifications` function in `commit.ts` to check if the object being proxied is an instance of `Date`. If it is, the function returns the object as is to avoid errors when JavaScript tries to convert the `Date` object to a string. This change improves the stability and reliability of the code.

--- packages/conventional-changelog-writer/src/commit.ts
+++ packages/conventional-changelog-writer/src/commit.ts
@@ -8,6 +8,13 @@ import type {
 } from './types/index.js'
 
 function preventModifications<T extends AnyObject>(object: T): T {
+  // This condition checks if the object is an instance of Date. The issue with Date objects arises when JavaScript tries to convert a Date object to a string (like when you're using template literals or calling toISOString). 
+  // JavaScript internally tries to access and modify some properties of the Date object during this conversion. However, if the Date object is made immutable by a function like preventModifications, these operations throw an error. 
+  // The error thrown is: "semantic-release: TypeError: Method Date.prototype.toString called on incompatible receiver [object Date]". 
+  // By returning the object if it's an instance of Date, we avoid this error.
+  if (object instanceof Date) {
+    return object;
+  }
   return new Proxy(object, {
     get(target, prop: string) {
       const value = target[prop] as unknown
