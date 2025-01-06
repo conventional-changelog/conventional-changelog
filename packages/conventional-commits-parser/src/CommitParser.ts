373d330c76c54de7b6214a27e5b1577b243ed029
Victor Korzunin <5180700+floydspace@users.noreply.github.com>
2025-01-06 14:45:22 +0100

    fix(conventional-commits-parser): give a possible 'header' merge pattern correspondence a priority over the next line (#1311)
    Co-authored-by: Dan Onoshko <danon0404@gmail.com>

--- packages/conventional-commits-parser/src/CommitParser.ts
+++ packages/conventional-commits-parser/src/CommitParser.ts
@@ -185,7 +185,7 @@ export class CommitParser {
 
     const { commit, options } = this
     const correspondence = options.headerCorrespondence || []
-    const header = this.nextLine()
+    const header = commit.header ?? this.nextLine()
     let matches: RegExpMatchArray | null = null
 
     if (header) {
